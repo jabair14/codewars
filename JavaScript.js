66807090c1e94856acff9434eeb3755769927b0b
Jabair <jabairk@gmail.com>
2021-11-09 16:26:47 -0500

    isTriangle done

--- JavaScript.js
+++ JavaScript.js
@@ -64,3 +64,18 @@ function maskify(cc) {
     
     return sumTotal; 
   }
+
+  //is Triangle 
+
+  function isTriangle(a,b,c){
+    if ((a + b <= c) || (a + c <= b) || (b + c <= a)){
+     return false;
+    } else {
+      if ((a <= 0) || (b <= 0) || (c <= 0)){
+        return false;
+      } else {
+        return true;
+      }
+    }
+    
+  }
