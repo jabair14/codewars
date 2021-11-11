636994a9c741c01de99892c2bdc02a2e98eec63a
Jabair <jabairk@gmail.com>
2021-11-11 11:53:45 -0500

    parity outlier

--- JavaScript.js
+++ JavaScript.js
@@ -122,3 +122,12 @@ function maskify(cc) {
     }
     return mergedArr.filter((value) => value !== undefined);
   }
+
+  //find parity outlier
+  function findOutlier(int){
+    var even = int.filter(a=>a%2==0);
+    var odd = int.filter(a=>a%2!==0);
+    return even.length==1? even[0] : odd[0];
+  }
+
+  
